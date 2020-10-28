b6cbf14dd8cf46a011ae892d797cf815b534d584
Mitchell Ponchione <mitchell.ponchione@protonmail.com>
2020-10-27 23:18:58 -0400

    Initial commit for chrome extension

---
+++ src/script/background.js
@@ -0,0 +1,13 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     var button = document.getElementById("findConns")
+//     button.addEventListener("click", (e) => {
+//         console.log(e)
+//     })
+// })
+
+// chrome.runtime.onInstalled.addListener(function () {
+//     chrome.storage.sync.set({color: '#3aa757'}, function () {
+//         console.log("The color is green.");
+//     });
+// });
+
